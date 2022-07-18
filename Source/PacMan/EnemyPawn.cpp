322d6944b7d215119d2510dab9c5497a5402f240
mm0203 <95344747+mm0203@users.noreply.github.com>
2022-07-19 04:05:33 +0900

    敵の実装

--- Source/PacMan/EnemyPawn.cpp
+++ Source/PacMan/EnemyPawn.cpp
@@ -6,18 +6,18 @@
 
 void AEnemyPawn::Hunt()
 {
-
+	// 探索
 	State = EEnemyState::Default;
 
+	// イベントディスパッチャーの呼び出し
 	StateChangedEvent.Broadcast(State);
-
 }
 
 void AEnemyPawn::Idle()
 {
-
+	// 待機
 	State = EEnemyState::Idle;
 
+	// イベントディスパッチャーの呼び出し
 	StateChangedEvent.Broadcast(State);
-
 }
--- Source/PacMan/EnemyPawn.h
+++ Source/PacMan/EnemyPawn.h
@@ -10,9 +10,10 @@
 UENUM(BlueprintType)
 enum class EEnemyState : uint8
 {
-	Default, // なし
+	Default, // 探索
 	Idle	 // 待機
 };
+
 // 動的マルチキャストデリゲート(イベントディスパッチャー)宣言 (型,引数名)
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEnemyStateChangedEvent, EEnemyState, NewState);
 
@@ -22,19 +23,24 @@ class PACMAN_API AEnemyPawn : public APawn
 	GENERATED_BODY()
 
 public:
+	// 敵の状態
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		EEnemyState State = EEnemyState::Default;
 
+	// 探索
 	UFUNCTION(BlueprintCallable)
 		void Hunt();
 
+	// 待機
 	UFUNCTION(BlueprintCallable)
 		void Idle();
 
+	// 状態変化(イベントディスパッチャー)
 	FEnemyStateChangedEvent& OnStateChanged() { return StateChangedEvent; }
 
 private:
 
+	// 動的マルチキャストデリゲート(イベントディスパッチャー)の定義
 	UPROPERTY(BlueprintAssignable, BlueprintCallable)
 		FEnemyStateChangedEvent StateChangedEvent;
 
